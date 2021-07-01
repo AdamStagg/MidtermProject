94b30055d3c0a9d6f76ed0cfb273f630b8d2ba1c
Adam Stagg <adam81501@gmail.com>
2021-07-01 10:26:29 -0400

    SceneTransitionManager created and implemented.
    Made into a singleton

--- Prototype/Bold Goats - Prototype/Assets/Scripts/GameManager.cs
+++ Prototype/Bold Goats - Prototype/Assets/Scripts/GameManager.cs
@@ -9,6 +9,7 @@ public class GameManager : MonoBehaviour
     private void Awake()
     {
         instance = this;
+        DontDestroyOnLoad(gameObject);
     }
 
     #region //Player
--- Prototype/Bold Goats - Prototype/Assets/Scripts/SceneTransitionManager.cs
+++ Prototype/Bold Goats - Prototype/Assets/Scripts/SceneTransitionManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneTransitionManager : MonoBehaviour
 {
@@ -9,6 +10,7 @@ public class SceneTransitionManager : MonoBehaviour
     private void Awake()
     {
         instance = this;
+        DontDestroyOnLoad(gameObject);
     }
 
     /*
@@ -16,39 +18,66 @@ public class SceneTransitionManager : MonoBehaviour
  */
     public void LoadScene(string _sceneName)
     {
-        throw new System.NotImplementedException();
+        SceneManager.LoadScene(_sceneName);
     }
     /*
      * Loads a scene with a fading transition, takes time to wait for the unload. Takes a string for the name of the scene to load.
      */
     public void LoadSceneAsync(string _sceneName)
     {
-        throw new System.NotImplementedException();
+        LoadSceneEnumerator(_sceneName);
     }
 
     IEnumerator LoadSceneEnumerator(string _sceneName)
     {
-        yield return null;
-        throw new System.NotImplementedException();
+        /*
+         * Fader code fade out here
+         */
+        
+        
+        AsyncOperation unload = SceneManager.UnloadSceneAsync(SceneManager.GetActiveScene());
+        AsyncOperation load = SceneManager.LoadSceneAsync(_sceneName);
+        while (!unload.isDone || !load.isDone)
+        {
+            yield return null;
+        }
+
+        /*
+         *  Fader code fade in here
+         */
+                
     }
 
     IEnumerator LoadSceneEnumerator(int _buildIndex)
     {
-        yield return null;
-        throw new System.NotImplementedException();
+        /*
+         * Fader code fade out here
+         */
+
+
+        AsyncOperation unload = SceneManager.UnloadSceneAsync(SceneManager.GetActiveScene());
+        AsyncOperation load = SceneManager.LoadSceneAsync(_buildIndex);
+        while (!unload.isDone || !load.isDone)
+        {
+            yield return null;
+        }
+
+        /*
+         *  Fader code fade in here
+         */
     }
     /*
     * Loads a scene without a transition, immediately switches to the new scene. Takes a integer for the build index of the scene to load.
     */
     public void LoadScene(int _buildIndex)
     {
-        throw new System.NotImplementedException();
+        SceneManager.LoadScene(_buildIndex);
     }
     /*
     * Loads a scene with a fading transition, takes time to wait for the unload. Takes a integer for the build index of the scene to load.
      */
     public void LoadSceneAsync(int _buildIndex)
     {
-        throw new System.NotImplementedException();
+        StartCoroutine(LoadSceneEnumerator(_buildIndex));
     }
 }
