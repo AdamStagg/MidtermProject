532614a932f1b8e921ac4c4c9f7a78605332a47f
Owens Nicolas <OJNicolas@student.fullsail.edu>
2021-07-01 14:37:01 -0400

    Owens: Enemy Waypoints

--- Prototype/Bold Goats - Prototype/Assets/Scripts/EnemyPathFind.cs
+++ Prototype/Bold Goats - Prototype/Assets/Scripts/EnemyPathFind.cs
@@ -5,36 +5,57 @@ using UnityEngine.AI;
 
 public class EnemyPathFind : MonoBehaviour
 {
-    [SerializeField]NavMeshAgent aiEnemy;
+    private NavMeshAgent aiEnemy;
     Vector3 startPos;
+    public Vector3[] gaurdPoints;
+    private int destinationPoint;
     public float roamRadius = 15.00f;
     GameObject player;
     // Start is called before the first frame update
     void Start()
     {
+        aiEnemy = GetComponent<NavMeshAgent>();
         player = GameObject.Find("Player");
         startPos = transform.position;
+
+        GoToNextPoint();
     }
+
+
 
     // Update is called once per frame
     void Update()
     {
-        aiEnemy.SetDestination(player.transform.position);
-        //if (aiEnemy.remainingDistance < .01f)
-        //{
-        //DirectionTest();
-
-        //}
+        //aiEnemy.SetDestination(player.transform.position);
+        if (aiEnemy.remainingDistance < 1.0f && !aiEnemy.pathPending)
+        {
+            GoToNextPoint();
+        }
     }
 
-    void DirectionTest()
+    void GoToNextPoint()
     {
-        Vector3 randomDirection = Random.insideUnitSphere * 10;
-        randomDirection += startPos;
-        NavMeshHit hit;
-        NavMesh.SamplePosition(randomDirection, out hit, roamRadius, 1);
-        NavMeshPath path = new NavMeshPath();
-        aiEnemy.SetPath(path);
+        // If there are no points set, No Need to run Function
+        if (gaurdPoints.Length == 0)
+        {
+            return;
+        }
 
+        // Set Gaurd point to the point currently selected
+        aiEnemy.destination = gaurdPoints[destinationPoint];
+
+        // Set Destination Point to next point
+        destinationPoint = (destinationPoint + 1) % gaurdPoints.Length;
     }
+
+    //void Patrol()
+    //{
+    //    Vector3 randomDirection = Random.insideUnitSphere * 10;
+    //    randomDirection += startPos;
+    //    NavMeshHit hit;
+    //    NavMesh.SamplePosition(randomDirection, out hit, roamRadius, 1);
+    //    NavMeshPath path = new NavMeshPath();
+    //    aiEnemy.SetPath(path);
+
+    //}
 }
