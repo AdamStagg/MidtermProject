e4a401dbdbf52ac43108075a2f4dc767f0e7d7eb
FleepyBeepy <85187244+FleepyBeepy@users.noreply.github.com>
2021-07-19 14:21:57 -0400

    Got crouch and run working

--- Prototype/Bold Goats - Prototype/Assets/Scripts/PlayerController.cs
+++ Prototype/Bold Goats - Prototype/Assets/Scripts/PlayerController.cs
@@ -5,32 +5,64 @@ using Enemy;
 
 public class PlayerController : MonoBehaviour
 {
-   
+    ///////////Basic Player Movement///////////
+    public Transform PlayerTransform;
     private CharacterController Controller;
-    public Transform EnemyTransform;
     private Vector3 PlayerVelocity;
     private bool GroundedPlayer;
     private bool Crouched = false;
+    private bool Running = false;
     public float PlayerSpeed = 5.0f;
     private float DistanceToEnemy;
     private float GravityValue = -9.81f;
-    private float ControllerHeight = .5f;
-    
+    private float ControllerHeight = .25f;
 
+
+    ///////////Variables for Attacking///////////
+    public Transform EnemyTransform;
     public Collider attackCollider;
 
+    ///////////Variables for Kunai///////////
     public Rigidbody Kunai;
     public int AmountOfKunais = 3;
+
+    ///////////Variables for Climbing///////////
+    /* public bool Climbing;
+     private bool InPosition;
+     private bool IsLerping;
+     private float PosT;
+     private float Delta;
+     public float PositionOffset;
+     public float OffsetFromWall = .3f;
+     public float SpeedMultiplier = .2f;
+     public float ClimbSpeed = 3f;
+     public float RotateSpeed = 5f;
+     Vector3 StartingPosition;
+     Vector3 TargetPosition;
+     Quaternion StartRotation;
+     Quaternion TargetRotation;
+     Transform Helper;
+     public Animator Anim;
+    */
+
+    ///////////Variables for Audio///////////
+    public AudioSource RunAudio;
+    public AudioSource WalkAudio;
 
 
     private void Start()
     {
         Controller = gameObject.AddComponent<CharacterController>();
-        attackCollider.enabled = false;        
+        attackCollider.enabled = false;
+       /* Helper = new GameObject().transform;
+        Helper.name = "Climb Helper";
+        CheckForClimb();
+       */
     }
 
     void Update()
     {
+
         GroundedPlayer = Controller.isGrounded;
 
         if (GroundedPlayer && PlayerVelocity.y < 0)
@@ -38,18 +70,18 @@ public class PlayerController : MonoBehaviour
             PlayerVelocity.y = 0f;
         }
 
+
+       // Delta = Time.deltaTime;
+       // Tick(Delta);
+
         ///////////Player movement (Left, Right, Forward, Bacward)///////////
         Vector3 Move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
 
-        
 
+        ///////////Camera Movement///////////
         Controller.Move(Camera.main.transform.right * Move.x * Time.deltaTime * PlayerSpeed);
         Controller.Move(Camera.main.transform.forward * Move.z * Time.deltaTime * PlayerSpeed);
-
-
-
-        //if (Move != Vector3.zero)
-        //{
+       
         transform.forward = Camera.main.transform.forward;
 
         Vector3 angles = transform.rotation.eulerAngles;
@@ -58,10 +90,15 @@ public class PlayerController : MonoBehaviour
 
         transform.rotation = Quaternion.Euler(angles);
 
-        //}
+        ///////////Player Run///////////
+        if (Input.GetButtonDown("Run")) 
+        {
+            Running = !Running;
+            Run();
+        }
 
         ///////////Player Crouch///////////
-        if (GroundedPlayer == true && Input.GetButtonDown("Crouch"))
+        if (Input.GetButtonDown("Crouch"))
         {
             Crouched = !Crouched;
             ToggleCrouch();
@@ -86,7 +123,7 @@ public class PlayerController : MonoBehaviour
             }
 
         }
-
+        
 
         ///////////Reloads Kunais///////////
         ///Will Change after reload stations are included
@@ -111,19 +148,21 @@ public class PlayerController : MonoBehaviour
 
         
 
-        
-
         PlayerVelocity.y += GravityValue * Time.deltaTime;
         Controller.Move(PlayerVelocity * Time.deltaTime);
 
     }
 
+
+    ///////////Support Methods///////////
+    ///Wait time for attack
     IEnumerator WaitSomeTime(float seconds)
     {
         yield return new WaitForSeconds(seconds);
        attackCollider.enabled = false;
     }
-
+  
+    ///Kill enemy when colliding with them
     public void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Enemy")
@@ -134,13 +173,139 @@ public class PlayerController : MonoBehaviour
         }
     }
 
+    ///Checking if out of collider
     private void OnTriggerExit(Collider other)
     {
 
         Debug.Log("Trigger exit with " + other.name);
     }
 
+    ///Tick method for climbing
+  /*  public void Tick(float delta) 
+    {
+        if (!InPosition) 
+        {
+            GetInPosition(delta);
+            return;
+        }
+
+        if (!IsLerping)
+        {
+            float Hor = Input.GetAxis("Horizontal");
+            float Vert = Input.GetAxis("Vertical");
+            float M = Mathf.Abs(Hor) + Mathf.Abs(Vert);
+
+            Vector3 H = Helper.right * Hor;
+            Vector3 V = Helper.up * Vert;
+            Vector3 Move = (H + V).normalized;
+
+            bool canMove = CanMove(Move);
+            if (!canMove || Move == Vector3.zero) 
+            {
+                return;
+            }
+
+            PosT = 0;
+            IsLerping = true;
+            StartingPosition = transform.position;
+            //Vector3 TP = Helper.position - transform.position;
+            TargetPosition = Helper.position;
+
+        }
+        else 
+        {
+            PosT += delta * ClimbSpeed;
+            if (PosT > 1) 
+            {
+                PosT = 1;
+                IsLerping = false;
+
+            }
+
+            Vector3 ClimbPosition = Vector3.Lerp(StartingPosition, TargetPosition, PosT);
+            transform.position = ClimbPosition;
+            transform.rotation = Quaternion.Slerp(transform.rotation, Helper.rotation, delta * RotateSpeed);
+        }
+
+
+    }
+    ///Checking if the Player can move on the wall
+    bool CanMove(Vector3 moveDir) 
+    {
+        Vector3 Origin = transform.position;
+        float Distance = PositionOffset;
+        Vector3 Direction = moveDir;
+        Debug.DrawRay(Origin, Direction * Distance, Color.red);
+        RaycastHit hit;
+
+        if (Physics.Raycast(Origin, Direction, out hit, Distance)) 
+        {
+            return false;
+        }
+
+        Origin += moveDir * Distance;
+        Direction = Helper.forward;
+        float Distance2 = .5f;
+
+        Debug.DrawRay(Origin, Direction * Distance2, Color.blue);
+
+        if (Physics.Raycast(Origin, Direction, out hit, Distance)) 
+        {
+            Helper.position = PosWithOffset(Origin, hit.point);
+            Helper.rotation = Quaternion.LookRotation(-hit.normal);
+            return true;
+        }
+
+        Origin += Direction * Distance2;
+        Direction = -Vector3.up;
+
+        Debug.DrawRay(Origin, Direction, Color.yellow);
+        if (Physics.Raycast(Origin, Direction, out hit, Distance2)) 
+        {
+            float angle = Vector3.Angle(Helper.up, hit.normal);
+            if (angle < 40) 
+            {
+                Helper.position = PosWithOffset(Origin, hit.point);
+                Helper.rotation = Quaternion.LookRotation(-hit.normal);
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    ///Putting the player in position to climb
+    void GetInPosition(float delta) 
+    {
+        PosT += delta;
+
+        if (PosT > 1) 
+        {
+            PosT = 1;
+            InPosition = true;
+
+            //enable the ik (idk something from the tutorial for climbing)
+        }
+
+        Vector3 TP = Vector3.Lerp(StartingPosition, TargetPosition, PosT);
+        transform.position = TP;
+        transform.rotation = Quaternion.Slerp(transform.rotation, Helper.rotation, delta * RotateSpeed);
+
+    }
+
+    ///Climbing slanted and crooked surfaces
+    Vector3 PosWithOffset(Vector3 origin, Vector3 target) 
+    {
+        Vector3 dir = origin - target;
+        dir.Normalize();
+        Vector3 offset = dir * OffsetFromWall;
+        return target + offset;
+    }
+  */
+
     ///////////Player Actions///////////
+
+    ///Throwing Kunais
     void CreateKunai()
     {
 
@@ -150,17 +315,20 @@ public class PlayerController : MonoBehaviour
 
     }
 
+    ///Reloading Kunais
     void Reload()
     {
         AmountOfKunais = 3;
         Debug.Log("Refilled Kunais");
     }
 
+    
+    ///Crouching
     void ToggleCrouch() 
     {
         if (Crouched == true)
         {
-            GameManager.Instance.Player.transform.localScale = new Vector3(1, .5f, 1);
+            PlayerTransform.transform.localScale = new Vector3(.25f, .1f, .25f);
             Controller.height = ControllerHeight;
             PlayerSpeed = 2.5f;
             Debug.Log("Player speed is now " + PlayerSpeed + " and the player is crouched");
@@ -172,10 +340,10 @@ public class PlayerController : MonoBehaviour
             ray.origin = transform.position;
             ray.direction = Vector3.up;
            
-            if (Physics.Raycast(GameManager.Instance.Player.transform.position, ray.direction, out hit, 2.2f))
+            if (Physics.Raycast(PlayerTransform.transform.position, ray.direction, out hit, 1.5f))
             {
-                GameManager.Instance.Player.transform.localScale = new Vector3(1, ControllerHeight, 1);
-                Controller.height = 1.8f;
+                PlayerTransform.transform.localScale = new Vector3(.25f, ControllerHeight, .25f);
+                Controller.height = .8f;
                 PlayerSpeed = 5f;
                 Debug.Log("Player is standing and the speed is " + PlayerSpeed); 
             }
@@ -186,6 +354,7 @@ public class PlayerController : MonoBehaviour
         }
     }
 
+    ///Attacking
     void ExecuteEnemy() 
     {
         //Perform Execution animation
@@ -194,6 +363,51 @@ public class PlayerController : MonoBehaviour
     
     }
 
+    void Run() 
+    {
+        if (Running && Crouched == false)
+        {
+            PlayerSpeed = 8.0f;
+            Debug.Log("Player is running");
+            
+        }
+        else 
+        {
+
+            Debug.Log("Player is walking");
+            PlayerSpeed = 5.0f;
+            
+        }
+        
+
+    }
+    
+
+   /* public void CheckForClimb() 
+    {
+        Vector3 origin = transform.position;
+        origin.y += 1.4f;
+        Vector3 direction = transform.forward;
+        RaycastHit hit;
+        if (Physics.Raycast(origin, direction, out hit, 1)) 
+        {
+            Helper.position = PosWithOffset(origin, hit.point);
+            ClimbOnWall(hit);
+        }
+    }
+
+    void ClimbOnWall(RaycastHit hit) 
+    {
+        GroundedPlayer = false;
+        Climbing = true;
+        Helper.transform.rotation = Quaternion.LookRotation(-hit.normal);
+        StartingPosition = transform.position;
+        TargetPosition = hit.point + (hit.normal * OffsetFromWall);
+        PosT = 0;
+        InPosition = false;
+        Anim.CrossFade("climb_idle", 2);
+    }
+   */
 }
 
 
