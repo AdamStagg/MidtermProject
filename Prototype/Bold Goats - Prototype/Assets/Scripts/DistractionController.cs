6b7576c921c83e1f03c07494367b70d4fb7f96b4
FleepyBeepy <85187244+FleepyBeepy@users.noreply.github.com>
2021-07-28 16:29:00 -0400

    No more errors in DistractionController

--- Prototype/Bold Goats - Prototype/Assets/Scripts/DistractionController.cs
+++ Prototype/Bold Goats - Prototype/Assets/Scripts/DistractionController.cs
@@ -25,26 +25,48 @@ public class DistractionController : MonoBehaviour
         }
         DistractableRb.isKinematic = true;
         explosion.SetActive(true);
-        Destroy(gameObject);
-        OnTriggerEnter(explosion.GetComponent<Collider>());
+        Destroy(gameObject, 2);
+        //OnTriggerEnter(explosion.GetComponent<Collider>());
+        for (int i = 0; i < enemies.Count; i++) 
+        {
+            enemies[i].transform.position = transform.position;
+        }
         
     }
 
     private void OnTriggerEnter(Collider other)
     {
+        other = explosion.GetComponent<Collider>();
+
         if (other.transform.parent != null && other.tag == "Enemy")
         {
             enemies.Add(transform.parent.GetComponent<EnemyState>());
 
         }
+
     }
 
-    private void OnTriggerExit(Collider other)
+    /*private void OnTriggerExit(Collider other)
     {
-        if (other.transform.parent!= null && other.transform.parent.tag == "Enemy")
+        other = explosion.GetComponent<Collider>();
+
+        if (other.transform.parent!= null && other.tag == "Enemy")
         {
             enemies.Remove(transform.parent.GetComponent<EnemyState>());
 
+        }
+    }
+    */
+
+    private void LateUpdate()
+    {
+       if (explosion == null && enemies.Count >=1) 
+        {
+            for (int i = 0; i < enemies.Count; i++) 
+            {
+                enemies.Remove(enemies[i]);
+                i--;
+            }
         }
     }
 
