b22a5df3953b13ee2891e00cac78a98a177a7bfb
Cameron Stewart <CDStewart2@student.fullsail.edu>
2021-08-09 13:01:54 -0400

    CutScenes Animations and Scripts

---
+++ Prototype/Bold Goats - Prototype/Assets/Scripts/AnimationController.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationController : MonoBehaviour
+{
+    [SerializeField] float Speed = 5f;
+    
+    Animator animator;
+    
+    // Start is called before the first frame update
+    void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Input 
+        float Horizontal = Input.GetAxis("Horizontal");
+        float Vertical = Input.GetAxis("Vertical");
+
+        //Movement
+        Vector3 Movement = new Vector3(Horizontal,0f,Vertical);
+
+        //Moving
+        if (Movement.magnitude > 0)
+        {
+            Movement.Normalize();
+            Movement *= Speed * Time.deltaTime;
+            transform.Translate(Movement, Space.World);
+        }
+
+        //Animation
+        float VelocityZ = Vector3.Dot(Movement.normalized, transform.forward);
+        float VelocityX = Vector3.Dot(Movement.normalized, transform.right);
+
+        animator.SetFloat("VelocityZ", VelocityZ, 0.1f, Time.deltaTime);
+        animator.SetFloat("VelocityX", VelocityX, 0.1f, Time.deltaTime);
+
+    }
+}
