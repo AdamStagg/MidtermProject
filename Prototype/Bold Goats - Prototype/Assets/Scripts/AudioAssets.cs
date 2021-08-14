a2af09a60e7222785f786bb866ccef97d266f264
Adam <adam81501@gmail.com>
2021-08-14 11:14:00 -0400

    Moved all the project files to the correct spot

---
+++ Prototype/Bold Goats - Prototype/Assets/Scripts/AudioAssets.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioAssets : MonoBehaviour
+{
+    public static AudioAssets instance;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        } else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+
+    public SoundAudioClip[] soundAudioClipArray;
+    public MusicAudioClip[] musicAudioClipArray;
+
+
+    [System.Serializable]
+    public class SoundAudioClip
+    {
+        public SoundManager.Sound sound;
+        public AudioClip audioClip;
+    }
+
+    [System.Serializable]
+    public class MusicAudioClip
+    {
+        public SoundManager.Music song;
+        public AudioClip audioClip;
+    }
+
+}
