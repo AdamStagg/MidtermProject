bcaa6296453a53485b7bbba43d1ebb2870d76884
FleepyBeepy <85187244+FleepyBeepy@users.noreply.github.com>
2021-08-23 13:11:51 -0400

    Got rid of conflicts in DistractionController

--- Prototype/Bold Goats - Prototype/Assets/Scripts/DistractionController.cs
+++ Prototype/Bold Goats - Prototype/Assets/Scripts/DistractionController.cs
@@ -21,23 +21,14 @@ public class DistractionController : MonoBehaviour
 
     private void OnCollisionEnter(Collision hit)
     {
-<<<<<<< HEAD
+
         //foreach (EnemyState enemy in enemies)
         //{
         //    enemy.GetComponent<EnemyPathFind>().SetInvestigatePosition(transform);
         //    enemy.InvokeInvestigate();
         //}
-=======
-        foreach (EnemyState enemy in enemies)
-        {
-            EnemyPathFind enemypf = enemy.GetComponent<EnemyPathFind>();
-            enemypf.SetInvestigatePosition(transform);
-            enemypf.lookingAround = false;
-            enemypf.StopAllCoroutines();
-            enemypf.ResumeNavigation();
-            enemy.InvokeInvestigate();
-        }
->>>>>>> 3dcea9a20bb384092677e6475edc88d9393f4950
+
+
         DistractableRb.isKinematic = true;
         explosion.SetActive(true);
         SoundManager.PlaySound(SoundManager.Sound.DistractionExplosion);
@@ -48,25 +39,14 @@ public class DistractionController : MonoBehaviour
     {
         if (other.transform.parent != null && other.transform.parent.tag == "Enemy")
         {
-            //GameObject enemy = other.transform.parent;
-            //enemies.Add(other.transform.parent.GetComponent<EnemyState>());
+            
             other.transform.parent.GetComponent<EnemyPathFind>().SetInvestigatePosition(transform);
             other.transform.parent.GetComponent<EnemyState>().InvokeInvestigate();
         }
 
     }
 
-    private void LateUpdate()
-    {
-       //if (explosion == null && enemies.Count >=1) 
-       // {
-       //     for (int i = 0; i < enemies.Count; i++) 
-       //     {
-       //         enemies.Remove(enemies[i]);
-       //         i--;
-       //     }
-       // }
-    }
+   
 
 
 }
